c8ed3a823fb995eb1165ab12cb7c5977ae286702
ashraful islam <developerashrafulislamrifaz@gmail.com>
2024-08-23 17:07:03 +0600

    added footer

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Components/Footer/Footer";
 
 const Layout = () => {
     return (
-        <div>
-            <div>Header</div>
-            <Outlet/>
-            <div>Footer</div>
+        <div className="min-h-screen">
+            <div className="max-w-[92%] lg:max-w-[1300px] mx-auto">
+                <div>Header</div>
+                <Outlet/>
+            </div>
+            <Footer />
         </div>
     );
 };
