451e90377d7efca639b7576849e9f7aed6a49708
ashraful islam <developerashrafulislamrifaz@gmail.com>
2024-08-23 17:32:06 +0600

    added category json file

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,39 @@
+import { NavLink } from 'react-router-dom';
+import Logo from '../../assets/logo.png'
+import { useState } from 'react';
+
+const navItems = [
+    {
+        name: 'indian',
+        link: '/'
+    },
+    {
+        name: 'bangladeshi',
+        link: '/bangladeshi'
+    },
+    {
+        name: 'category',
+        link: '/category'
+    },
+]
 
 const Header = () => {
+    const [currentNav, setCurrentNav] = useState('indian')
+
     return (
-        <div>
-            
+        <div className="header">
+            <img src={Logo} className='logo' alt="logo" />
+            <div>
+                <ul>
+                    {
+                        navItems?.map((item, idx) => (
+                            <li key={idx} onClick={() => setCurrentNav(item?.name)}>
+                                <a href={item.link}>{item.name}</a>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </div>
         </div>
     );
 };
--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../Components/Footer/Footer";
+import Header from "../Components/Header/Header";
 
 const Layout = () => {
     return (
-        <div className="min-h-screen">
+        <div className="min-h-screen h-full">
             <div className="max-w-[92%] lg:max-w-[1300px] mx-auto">
-                <div>Header</div>
+                <Header />
                 <Outlet/>
             </div>
             <Footer />
